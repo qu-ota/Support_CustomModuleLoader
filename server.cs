4dabdab4056af3d8110212a871af2ded3fceca7b
dom <34490428+qu-ota@users.noreply.github.com>
2018-12-18 15:53:47 -0500

    Update server.cs

--- server.cs
+++ server.cs
@@ -1,3 +1,17 @@
 //Custom Module Loader by Dominoes
 //Put all scripts into the "modules" folder, then use reloadCMLmodules(); in console to refresh the scripts loaded.
 
+function loadYAMAModules() //function for finding & loading modules
+{
+	%search = "Add-Ons/Support_CustomModuleLoader/modules/*.cs";
+
+	for(%file = findFirstFile(%search); isFile(%file); %file = findNextFile(%search))
+	{
+		echo("=== Module Loader | Loading module:" SPC %file SPC "===");
+		exec(%file);
+	}
+}
+echo("=== Module Loader Initiated ===");
+echo("=== Module Loader | Looking for modules ===");
+loadYAMAModules();
+echo("=== Module Loader | Finished loading ===");
